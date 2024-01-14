3761dbddd28e179bc18342b34c9751dcf06eada7
JChartier001 <JChartier001@gmail.com>
2024-01-14 16:09:29 -0500

    auth callbacks

--- auth.ts
+++ auth.ts
@@ -6,6 +6,12 @@ import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
 import { getUserById } from '@/data/user';
 
+declare module 'next-auth' {
+	interface User {
+		role: 'ADMIN' | 'USER';
+	}
+}
+
 export const {
 	handlers: { GET, POST },
 	auth,
@@ -13,11 +19,35 @@ export const {
 	signOut,
 	update,
 } = NextAuth({
+	callbacks: {
+		// async signIn({ user }) {
+		// 	const existingUser = await getUserById(user.id);
+		// 	if (!existingUser || !existingUser.emailVerified) return false;
+		// 	return true
+		// },
+
+		async session({ session, token }) {
+			return {
+				...session,
+				id: token.sub,
+				role: token.role,
+			};
+		},
+		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+			if (!existingUser) return token;
+			token.role = existingUser.role;
+
+			return token;
+		},
+	},
 	pages: {
 		signIn: '/auth/login',
 		error: '/auth/error',
 	},
-	
+
 	adapter: PrismaAdapter(db),
 	session: { strategy: 'jwt' },
 	...authConfig,
